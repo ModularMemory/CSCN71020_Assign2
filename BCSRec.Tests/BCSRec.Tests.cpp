0883c292b077b82633438d8eaa36469877c61b5d
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-10-09 17:30:42 -0400

    Add test project

---
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "..\BCSRec\main.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BCSRecTests
+{
+	TEST_CLASS(BCSRecTests)
+	{
+	public:
+		
+		TEST_METHOD(Test1)
+		{
+		}
+	};
+}
