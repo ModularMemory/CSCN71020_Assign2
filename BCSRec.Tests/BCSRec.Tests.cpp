0cf66c96b768c6ee54d48d0063e0902bd65d287d
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-10-09 18:25:40 -0400

    Add perimeter & area tests

--- BCSRec.Tests/BCSRec.Tests.cpp
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -9,12 +9,30 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BCSRecTests
 {
-	TEST_CLASS(BCSRecTests)
+	TEST_CLASS(RectangleTests)
 	{
 	public:
 		
-		TEST_METHOD(Test1)
+		TEST_METHOD(getPerimeter_7and3_equals20)
 		{
+			int length = 7;
+			int width = 3;
+			const int expected = 20;
+
+			int actual = getPerimeter(&length, &width);
+
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(getArea_7and3_equals21)
+		{
+			int length = 7;
+			int width = 3;
+			const int expected = 21;
+
+			int actual = getArea(&length, &width);
+
+			Assert::AreEqual(expected, actual);
 		}
 	};
 }
